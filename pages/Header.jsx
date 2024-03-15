15ca5b1e29e941fb77faf7afa4dc193d9b4f11c2
James Yu <james.yuu@hotmail.com>
2024-03-14 20:10:26 -0400

    second commit

---
+++ pages/Header.jsx
@@ -0,0 +1,15 @@
+
+
+function Header(){
+    
+    return(
+       <div>
+            <h1 className="header-title">Spin City</h1>
+         <img className="img-edit"src='https://static.vecteezy.com/system/resources/thumbnails/016/612/216/small_2x/table-tennis-icon-free-vector.jpg' alt='image'></img>
+         <h1 className="header-style">Welcome to Spin City! Buy Lightly Used Table Tennis Rubbers for Cheap</h1>
+         
+        </div>
+    );
+}
+
+export default Header
